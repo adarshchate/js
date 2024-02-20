88683dc7ed4fde51f0ae2afea95dc16d96e71055
adarsh chate <85831144+adarshchate@users.noreply.github.com>
2024-02-20 17:26:29 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 7777
+let accountEmail = "chateadarsh0144@gmail.com"
+var accountPass = "12345"
+accountCity = "Pune"
+let accountState;
+
+// accountId = 88   /// not allowed const variable
+accountEmail = "test@gmail.com"
+accountPass = "abc"
+accountCity = "Mumbai"
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPass, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+let name = "Adarsh"
+let age = 24
+let isLogin = false
+
+// number 
+// bigint
+// string => ""
+// boolean => true/false
+// null =>  standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
